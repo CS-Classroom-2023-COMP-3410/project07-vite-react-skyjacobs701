1ed49113b5b5fa1a533f133bd988f461965960f9
Skylar Jacobs <skylar.jacobs@du.edu>
2026-02-18 03:09:34 +0000

    Updated package.json and vite.config.js to deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        // You can change this if needed
-    },
+  plugins: [react()],
+  base: '/project07-vite-react-skyjacobs701/'
 });
